333931ef1da15025b6e9c4a9930326158e9fb83e
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2021-01-07 09:07:27 +0530

    Commented on everything...
    This code is still under improvement...if you wanna help out...pls do so...

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 """
-Code written by: Sid The Loser
+Code written by: Sidharth S
+
 Refrences from:
     https://en.wikipedia.org/wiki/3D_projection#:~:text=A%203D%20projection%20(or%20graphical,capability%20on%20a%20simpler%20plane.
     https://en.wikipedia.org/wiki/Rotation_matrix
+
+Description from the developer:
+    This code took me 2 hours to finish and the whole code was done in a rush...so...I'm sorry if you are having a hard time reading it.
 """
 
-import turtle
-import math
-
-def projection_matrix(coords):
-    return coords[0], coords[1]
+import turtle # This just imports turtle library
+import math # This just imports math library
 
 screen = turtle.Screen()
 screen.title("")
@@ -22,7 +23,7 @@ pen.color("white")
 pen.penup()
 pen.speed(0)
 
-def draw_point(x, y):
+def draw_point(x, y): # This is a defenition made just to draw a point on the screen...
     pen.penup()
     pen.goto(x, y)
     pen.pendown()
@@ -32,18 +33,17 @@ def draw_point(x, y):
     pen.pensize(1)
     pen.penup()
 
-def draw_line(coord, coord1):
+def draw_line(coord, coord1): # This is a defenition made just to draw a line on the screen...
     pen.penup()
     pen.goto(coord)
     pen.pendown()
     pen.goto(coord1)
     pen.penup()
 
-class CustomError(Exception):
+class CustomError(Exception): # This class is for me to show custom errors
     pass
 
-def rotateX(angle, coords):
-    coords = list(coords)
+def rotateX(angle, coords): # This defention does the math to rotate X axis in the 3D coordinates that we pass in...
     X = [
 
         [1, 0, 0],
@@ -51,19 +51,28 @@ def rotateX(angle, coords):
         [0, math.sin(angle), math.cos(angle)]
 
         ]
+
+    """
+    The X on the top there just defines this:
+
+        [ 1,    0,    0]
+        [ 0, cosθ, sinθ]
+        [ 0, sinθ, cosθ]
+
+    """
+
     if len(coords) == 3:
         result = []
 
-        for i in range(len(X)):
+        for i in range(len(X)): # This part of the code does the multiplication...
             result.append((X[i][0]*coords[0])+(X[i][1]*coords[1])+(X[i][2]*coords[2]))
 
-        return result
+        return result # Returns the result that we get from the rotation matrix...
 
     else:
-        raise CustomError("'rotateX', function only works with 3D coordinates")
+        raise CustomError("'rotateX', function only works with 3D coordinates") # This just raises an error if the length of coord list is not equal to 3...
 
-def rotateY(angle, coords):
-    coords = list(coords)
+def rotateY(angle, coords): # This defention does the math to rotate Y axis in the 3D coordinates that we pass in...
     X = [
 
         [math.cos(angle), 0, math.sin(angle)],
@@ -71,19 +80,28 @@ def rotateY(angle, coords):
         [-math.sin(angle), 0, math.cos(angle)]
 
         ]
+
+    """
+    The X on the top there just defines this:
+
+        [ cosθ, 0, sinθ]
+        [    0, 1,    0]
+        [-sinθ, 0, cosθ]
+
+    """
+
     if len(coords) == 3:
         result = []
 
-        for i in range(len(X)):
+        for i in range(len(X)): # This part of the code does the multiplication...
             result.append((X[i][0]*coords[0])+(X[i][1]*coords[1])+(X[i][2]*coords[2]))
 
-        return result
+        return result # Returns the result that we get from the rotation matrix...
 
     else:
-        raise CustomError("'rotateY', function only works with 3D coordinates")
+        raise CustomError("'rotateY', function only works with 3D coordinates") # This just raises an error if the length of coord list is not equal to 3...
 
-def rotateZ(angle, coords):
-    coords = list(coords)
+def rotateZ(angle, coords): # This defention does the math to rotate Z axis in the 3D coordinates that we pass in...
     X = [
 
         [math.cos(angle), -math.sin(angle), 0],
@@ -91,29 +109,48 @@ def rotateZ(angle, coords):
         [0, 0, 1]
 
         ]
+
+    """
+    The X on the top there just defines this:
+
+        [ cosθ, -sinθ, 0]
+        [ sinθ,  cosθ, 0]
+        [    0,     0, 1]
+
+    """
+
     if len(coords) == 3:
         result = []
 
-        for i in range(len(X)):
+        for i in range(len(X)): # This part of the code does the multiplication...
             result.append((X[i][0]*coords[0])+(X[i][1]*coords[1])+(X[i][2]*coords[2]))
 
-        return result
+        return result # Returns the result that we get from the rotation matrix...
 
     else:
-        raise CustomError("'rotateZ', function only works with 3D coordinates")
+        raise CustomError("'rotateZ', function only works with 3D coordinates") # This just raises an error if the length of coord list is not equal to 3...
 
-def conv_coords(coords):
+def conv_coords(coords): # This defenition is to covert 3D coordinates to 2D coordinates...
     if len(coords) == 3:
         return coords[0], coords[1]
+        """This does the same thing as projection matrix but easier:
+
+            Projection Matrix:
+
+                [1, 0, 0]
+                [0, 1, 0] * [12, 30, 15] = [12, 30]
+                [0, 0, 0]
+
+            My code:
+
+                conv_coords([12, 30, 15]) --> Haha just return the X and Y coordinates --> [12, 30]
+
+        """
 
     else:
-        raise CustomError("'conv_coords', function only works with 3D coordinates")
+        raise CustomError("'conv_coords', function only works with 3D coordinates") # This just raises an error if the length of coord list is not equal to 3...
 
-zoom = 50
-
-#You can change the vert and edges values with anything else that you would want...that means..this thing can draw more than a cube...INFINITE possibilities
-
-verts = (
+verts = ( # This list contains all the 3D coordinates for the verts of the cube...
     (1, -1, -1),
     (1, 1, -1),
     (-1, 1, -1),
@@ -124,7 +161,7 @@ verts = (
     (-1, 1, 1)
     )
 
-edges = (
+edges = ( # This is the list that stores which all verts to connect to form the edges of the cube...
     (0,1),
     (0,3),
     (0,4),
@@ -139,27 +176,34 @@ edges = (
     (5,7)
     )
 
-angle = 0
+zoom = 50 # This is some lazy way to englarge the cube's size...this will be changes soon...
+
+angle = 0 # This is another lazy code that I did...this will also be changes in the next version...but currntly stores the rotation of the cube...
 
 while True:
     try:
         screen.update()
 
-        pen.clear()
+        pen.clear() # This clears what the pen drew before...
 
-        vert_list = []
+        vert_list = [] # This is to store the 2D coordiates of the cube verts for the edges code to refrence off of...
 
         for vert in verts:
-            x, y = conv_coords(rotateX(angle, rotateY(angle, rotateZ(angle, vert))))
-            draw_point(x*zoom, y*zoom)
-            vert_list.append([x*zoom, y*zoom])
+            x, y = conv_coords(rotateX(angle, rotateY(angle, rotateZ(angle, vert)))) #This line of code rotates the cube vert coordinates and converts them to 2D coordinates for the turtle to display them...
+            draw_point(x*zoom, y*zoom) # This just draws a point on the screen using the X and Y coordinates from the complex code given above...
+            vert_list.append([x*zoom, y*zoom]) # This is for the edges code given below to reference the 2D coordinates that we get from the cube verts...
 
         for i in range(len(edges)):
-            draw_line(vert_list[edges[i][0]], vert_list[edges[i][1]])
+            draw_line(vert_list[edges[i][0]], vert_list[edges[i][1]]) # This is the code that we use to connect all the edges of the cube...
 
-        angle += 0.001
+        angle += 0.001 # This just adds 0.001 to angle
 
-    except:
+    except Exception as e: # This type of canvas update error managment was taken from my other repository: https://github.com/sid-the-loser/turtle-common-update-error/ ...
+        print(e)
         break
 
 quit()
+
+"""
+If there is any problem with my code pls report it in the issues tab...or at me on twitter @sid_loser...or DM me on instagram on @sid_the_loser...
+"""
